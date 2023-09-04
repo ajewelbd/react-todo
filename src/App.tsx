4a80902b4e3d0d74a7e67cd14aa6e69cd72f162e
Md. Ashraful Alam <ajewel@schertech.com>
2023-09-04 18:55:01 +0600

    feat: initial feature added

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,68 @@
+import { FormEvent, useState } from "react";
+
 export default function App() {
-	return "Hi";
+	const [newItem, setNewItem] = useState("");
+	const [todos, setTodos] = useState<Array<Todo>>([]);
+
+
+	function addItem(e: FormEvent) {
+		e.preventDefault();
+		if(newItem === "") return;
+		setTodos((currentTodos) => {
+			return [...currentTodos, {
+				id: crypto.randomUUID(),
+				title: newItem,
+				completed: false
+			}]
+		})
+
+		setNewItem("")
+	}
+
+	function toggleTodo(id: string, completed: boolean) {
+		setTodos(currentTodos => {
+			return currentTodos.map((todo: Todo) => {
+				if(todo.id === id) todo.completed = completed;
+				return todo;
+			})
+		})
+	}
+
+	function deleteTodo(todoId: string) {
+		setTodos(currentTodos => currentTodos.filter(({id}) => id !== todoId))
+	}
+
+	return (
+		<>
+			<div>
+				<h1>ToDo's</h1>
+				<form onSubmit={addItem}>
+					<label htmlFor="title">Title</label>
+					<input type="text" name="title" id="title" value={newItem} onChange={(e) => setNewItem(e.target.value)} />
+					<button type="submit">Add</button>
+				</form>
+			</div>
+			<div className="todo-list">
+				<ul>
+					{todos.map((todo: Todo) => {
+						return (
+							<li key={todo.id}>
+								<label htmlFor="todo">
+									<input type="checkbox" checked={todo.completed} onChange={(e) => toggleTodo(todo.id, e.target.checked)} />
+									{todo.title}
+								</label>
+								<button type="button" onClick={() => deleteTodo(todo.id)}>Delete</button>
+							</li>
+						)
+					})}
+				</ul>
+			</div>
+		</>
+	);
+}
+
+interface Todo {
+	id: string;
+	title: string;
+	completed: boolean;
 }
