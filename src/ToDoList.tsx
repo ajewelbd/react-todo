87396abc865df4ee24fbc98d1a33cbce3320db44
Md. Ashraful Alam <ajewel@schertech.com>
2023-09-05 19:36:21 +0600

    feat: styles added

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -3,20 +3,35 @@ import ToDoItem from "./ToDoItem";
 
 export default function ToDoList({ todos, toggleTodo, deleteTodo }: props) {
     return (
-        <div className="todo-list">
+        <div className="todo-list mt-8">
             {!todos.length && <p>No Todos available!</p>}
-            <ul>
-                {todos.map((todo: ToDo) => {
-                    return (
-                        <ToDoItem
-                            key={todo.id}
-                            todo={todo}
-                            toggleTodo={toggleTodo}
-                            deleteTodo={deleteTodo}
-                        />
-                    );
-                })}
-            </ul>
+            <table className="border-collapse border border-slate-500">
+                <thead className="bg-slate-50 dark:bg-slate-70">
+                    <tr>
+                        <th className="w-1/3 border border-slate-300 font-semibold p-4 text-slate-90 text-left">
+                            Completed
+                        </th>
+                        <th className="w-1/3 border border-slate-300 font-semibold p-4 text-slate-90 text-left">
+                            Title
+                        </th>
+                        <th className="w-1/3 border border-slate-300 font-semibold p-4 text-slate-90 text-left">
+                            Action
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {todos.map((todo: ToDo) => {
+                        return (
+                            <ToDoItem
+                                key={todo.id}
+                                todo={todo}
+                                toggleTodo={toggleTodo}
+                                deleteTodo={deleteTodo}
+                            />
+                        );
+                    })}
+                </tbody>
+            </table>
         </div>
     );
 }
